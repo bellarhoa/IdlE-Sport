9f4f319865e532a26a6447d5ce46f1fd75595da8
Bella Rhobiatul Adhawiyah <bellarhobiatuladawiyah@yahoo.co.id>
2019-12-01 17:58:09 +0700

    main-first

---
+++ app/src/main/java/com/bellarhoa/idle_sport/FragmentAtas.java
@@ -0,0 +1,23 @@
+package com.bellarhoa.idle_sport;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentAtas extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_atas, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedState) {
+        super.onActivityCreated(savedState);
+
+
+    }
+}
---
+++ app/src/main/java/com/bellarhoa/idle_sport/MainActivity.java
@@ -0,0 +1,65 @@
+package com.bellarhoa.idle_sport;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentManager frgManager;
+    private FragmentTransaction frgtransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        frgManager = getSupportFragmentManager();
+        frgtransaction = frgManager.beginTransaction();
+        Bundle bundle = new Bundle();
+        FragmentAtas fragmentAtas = new FragmentAtas();
+        fragmentAtas.setArguments(bundle);
+        frgtransaction.add(R.id.containerAtas, fragmentAtas);
+        frgtransaction.addToBackStack(null);
+        frgtransaction.commit();
+
+        frgManager = getSupportFragmentManager();
+        frgtransaction = frgManager.beginTransaction();
+        Bundle bundle2 = new Bundle();
+        FragmentBawah fragmentBawah = new FragmentBawah();
+        fragmentBawah.setArguments(bundle2);
+        frgtransaction.add(R.id.containerBawah, fragmentBawah);
+        frgtransaction.addToBackStack(null);
+        frgtransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
