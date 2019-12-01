b04f41d87deef130652b891284671e35db852155
Bella Rhobiatul Adhawiyah <bellarhobiatuladawiyah@yahoo.co.id>
2019-12-01 22:39:03 +0700

    thread-point-dan-tap

---
+++ app/src/main/java/com/bellarhoa/idle_sport/DataUpgrade.java
@@ -0,0 +1,37 @@
+package com.bellarhoa.idle_sport;
+
+public class DataUpgrade {
+    private String nama;
+    private int level, harga;
+
+    public DataUpgrade(String nama, int level, int harga) {
+        setNama(nama);
+        setLevel(level);
+        setHarga(harga);
+    }
+
+    public void setNama(String a){
+        this.nama = a;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public void setLevel(int b){
+        this.level = b;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public void setHarga(int c){
+        this.harga = c;
+    }
+
+    public int getHarga(){
+        return harga;
+    }
+
+}
--- app/src/main/java/com/bellarhoa/idle_sport/FragmentAtas.java
+++ app/src/main/java/com/bellarhoa/idle_sport/FragmentAtas.java
@@ -1,13 +1,20 @@
 package com.bellarhoa.idle_sport;
 
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class FragmentAtas extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
@@ -18,6 +25,63 @@ public class FragmentAtas extends Fragment {
     public void onActivityCreated(Bundle savedState) {
         super.onActivityCreated(savedState);
 
+        final TextView textViewPoint = getActivity().findViewById(R.id.textView_point);
+        final ProgressBar progBar = getActivity().findViewById(R.id.progBar);
+        ImageView gambarBackground = getActivity().findViewById(R.id.imageBackground);
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int i = 0;
+                    while (true) {
+                        i++;
+                        Thread.sleep(1000);
+
+                        //Untuk lihat apa thread jalan atau tidak di "Run"
+//                        Log.i("not dead", "not dead" + i);
+
+                        progBar.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                SharedPreferences dataSP = getActivity().getSharedPreferences("dataSP",0);
+                                int currPoint = dataSP.getInt("keyPoint", 0) + 10;
+                                textViewPoint.setText(String.valueOf(currPoint));
+
+                                SharedPreferences.Editor editData = dataSP.edit();
+                                editData.putInt("keyPoint", currPoint);
+                                editData.apply();
+                            }
+                        });
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        gambarBackground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Hasil Tap default aja = 5, sementara pakai ini
+                SharedPreferences dataSP = getActivity().getSharedPreferences("dataSP", 0);
+                int currPoint = dataSP.getInt("keyPoint", 0);
+                textViewPoint.setText(String.valueOf(currPoint+5) + "\t+5");
+
+                SharedPreferences.Editor editData = dataSP.edit();
+                editData.putInt("keyPoint", currPoint);
+                editData.apply();
+
+                //Ambil hasil Tap dari level upgrade
+//                SharedPreferences dataSP = getSharedPreferences("dataSP", 0);
+//                int currPoint = dataSP.getInt("keyPoint", 0) + dataSP.getInt("keyTap", 0);
+//                textViewPoint.setText(String.valueOf(currPoint));
+//
+//                SharedPreferences.Editor editData = dataSP.edit();
+//                editData.putInt("keyPoint", currPoint);
+//                editData.apply();
+            }
+        });
     }
 }
